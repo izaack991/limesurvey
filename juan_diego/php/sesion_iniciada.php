36341408b845eae4543799ce289b17ab90709b1f
Isaac Machado <isaac991mg@gmail.com>
2024-02-14 15:04:26 -0700

    Restriccion alumno-profesor

--- juan_diego/php/sesion_iniciada.php
+++ juan_diego/php/sesion_iniciada.php
@@ -11,11 +11,18 @@ $emaildominio = explode('@', $correo);
 $_SESSION['emaildominio'] = $emaildominio[1];
 $_SESSION['emailusuario'] = $emaildominio[0];
 
+$usuariovalidacion = explode('.', $emaildominio[0]);
+
 $validacion = $emaildominio[1];
 
-if ($validacion == 'uais.edu.mx' ) {
+if ($validacion == 'uais.edu.mx' || 'gmail.com' ) {
     
-    header('Location: Pagina_Docente.php');
+    if ($usuariovalidacion[1] == true) {
+        header('Location: Pagina_Docente.php');
+    } else {
+        header('Location: Pagina_Alumno.php');
+    }
+    
 } else {
     echo '"<script language="javascript">alert("Solo puedes ingresar con un correo institucional UAIM");window.location.href="../../login/login.php"</script>"';
 }
